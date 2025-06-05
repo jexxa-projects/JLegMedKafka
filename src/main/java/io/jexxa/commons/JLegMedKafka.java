3bc5d97f0664c24a89a4788e515268d01e70e816
repplix <116941865+repplix@users.noreply.github.com>
2025-06-05 10:57:19 +0200

    Added first version of sending messages to kafka from JLegMed flowgraph

--- src/main/java/io/jexxa/commons/JLegMedKafka.java
+++ src/main/java/io/jexxa/commons/JLegMedKafka.java
@@ -1,20 +1,38 @@
 package io.jexxa.commons;
 
-import io.jexxa.common.facade.logger.SLF4jLogger;
+import io.jexxa.jlegmed.core.JLegMed;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static io.jexxa.common.facade.logger.SLF4jLogger.getLogger;
+import static io.jexxa.jlegmed.plugins.monitor.LogMonitor.logFunctionStyle;
 
 public final class JLegMedKafka
 {
     public static void main(String[] args)
     {
 
-        SLF4jLogger.getLogger(JLegMedKafka.class).info("I am a Kafka Producer");
+        getLogger(JLegMedKafka.class).info("I am a Kafka Producer");
+        var jLegMed = new JLegMed(JLegMedKafka.class);
+        jLegMed.newFlowGraph("HelloKafka")
+                .every(1, TimeUnit.SECONDS)
 
+                .receive(String.class).from( () -> "Hello " )
+                .and().processWith(data -> data + "World" )
+                .and().consumeWith(JLegMedKafka::sendToKafka);
+
+        jLegMed.monitorPipes("HelloKafka", logFunctionStyle());
+        jLegMed.run();
+    }
+
+
+    private static void sendToKafka(String message)
+    {
         String bootstrapServers = "127.0.0.1:9092";
 
         // create Producer properties
@@ -28,7 +46,7 @@ public final class JLegMedKafka
 
         // create a producer record
         ProducerRecord<String, String> producerRecord =
-                new ProducerRecord<>("demo_java", "hello world");
+                new ProducerRecord<>("demo_java", message);
 
         // send data - asynchronous
         producer.send(producerRecord);
@@ -37,17 +55,6 @@ public final class JLegMedKafka
         producer.flush();
         // flush and close producer
         producer.close();
-
-
-       /*
-               var jLegMed = new JLegMed(JLegMedKafka.class);
-                jLegMed.newFlowGraph("HelloWorld")
-                .every(1, TimeUnit.SECONDS)
-
-                .receive(String.class).from( () -> "Hello " )
-                .and().processWith(data -> data + "World" )
-                .and().consumeWith(data -> getLogger(JLegMedKafka.class).info(data));
-
-        jLegMed.run();*/
     }
+
 }
