7bb32d95aab656693e71d497d3bbdbc6c6b10c28
repplix <116941865+repplix@users.noreply.github.com>
2025-06-06 09:38:38 +0200

    * Moved KafkaSender into a separate class * Corrected package name * Adjusted logging style

---
+++ src/main/java/io/jexxa/jlegmedkafka/JLegMedKafka.java
@@ -0,0 +1,29 @@
+package io.jexxa.jlegmedkafka;
+
+import io.jexxa.jlegmed.core.JLegMed;
+import io.jexxa.jlegmedkafka.plugins.event.KafkaSender;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.jexxa.common.facade.logger.SLF4jLogger.getLogger;
+import static io.jexxa.jlegmed.plugins.monitor.LogMonitor.logFunctionStyle;
+
+public final class JLegMedKafka
+{
+    public static void main(String[] args)
+    {
+
+        getLogger(JLegMedKafka.class).info("I am a Kafka Producer");
+        var jLegMed = new JLegMed(JLegMedKafka.class);
+        jLegMed.newFlowGraph("HelloKafka")
+                .every(1, TimeUnit.SECONDS)
+
+                .receive(String.class).from( () -> "Hello " )
+                .and().processWith(data -> data + "World" )
+                .and().consumeWith(KafkaSender::sendToKafka);
+
+        jLegMed.monitorPipes("HelloKafka", logFunctionStyle());
+        jLegMed.run();
+    }
+
+}
--- src/main/java/io/jexxa/jlegmedkafka/plugins/event/KafkaSender.java
+++ src/main/java/io/jexxa/jlegmedkafka/plugins/event/KafkaSender.java
@@ -1,38 +1,15 @@
-package io.jexxa.commons;
+package io.jexxa.jlegmedkafka.plugins.event;
 
-import io.jexxa.jlegmed.core.JLegMed;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
-import static io.jexxa.common.facade.logger.SLF4jLogger.getLogger;
-import static io.jexxa.jlegmed.plugins.monitor.LogMonitor.logFunctionStyle;
+public class KafkaSender {
 
-public final class JLegMedKafka
-{
-    public static void main(String[] args)
-    {
-
-        getLogger(JLegMedKafka.class).info("I am a Kafka Producer");
-        var jLegMed = new JLegMed(JLegMedKafka.class);
-        jLegMed.newFlowGraph("HelloKafka")
-                .every(1, TimeUnit.SECONDS)
-
-                .receive(String.class).from( () -> "Hello " )
-                .and().processWith(data -> data + "World" )
-                .and().consumeWith(JLegMedKafka::sendToKafka);
-
-        jLegMed.monitorPipes("HelloKafka", logFunctionStyle());
-        jLegMed.run();
-    }
-
-
-    private static void sendToKafka(String message)
-    {
+    public static void sendToKafka(String message) {
         String bootstrapServers = "127.0.0.1:9092";
 
         // create Producer properties
@@ -57,4 +34,5 @@ public final class JLegMedKafka
         producer.close();
     }
 
+    private KafkaSender() {}
 }
